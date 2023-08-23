4823f6842426680b4db76be084ca70aa9c4f834c
rhdehgks <rhdehgks@naver.com>
2023-08-23 21:50:51 +0900

    유저 목록 및 회비 기간 API 추가

--- api/admin/notification/notification.py
+++ api/admin/notification/notification.py
@@ -8,6 +8,12 @@ notification = Namespace('notification')
 # 알림 category
 NOTIFICATION_CATEGORY = {0: '정기 회의', 1: '디자인 파트 회의', 2: '아트 파트 회의', 3: '프로그래밍 파트 회의', 4: '청소', 5: '기타'}
 
+# 알림 대상자 category
+MEMBER_CATEGORY = {0: '활동 중인 회원 전체', 1: '활동 중인 정회원', 2: '활동 중인 수습회원', 3: '기타 선택'}
+
+# 요일 category
+DAY_CATEGORY = {0: '월요일', 1: '화요일', 2: '수요일', 3: '목요일', 4: '금요일', 5: '토요일', 6: '일요일'}
+
 # index 데이터를 문자열로 변경
 def convert_to_string(dictionary, index):
     return dictionary.get(index, None)
@@ -33,11 +39,13 @@ class NotificationByCategoryAPI(Resource):
             return [], 200
         else:
             for idx, notification in enumerate(notification_list):
-                # time, date, category를 문자열로 변경
+                # time, date, category, day를 문자열로 변경
                 notification_list[idx]['time'] = str(notification['time'])
                 notification_list[idx]['start_date'] = notification['start_date'].strftime('%Y-%m-%d')
                 notification_list[idx]['end_date'] = notification['end_date'].strftime('%Y-%m-%d')
                 notification_list[idx]['category'] = convert_to_string(NOTIFICATION_CATEGORY, notification['category'])
+                notification_list[idx]['member_category'] = convert_to_string(MEMBER_CATEGORY, notification['member_category'])
+                notification_list[idx]['day'] = convert_to_string(DAY_CATEGORY, notification['day'])
 
                 # DB에서 알림 대상자 목록 가져오기
                 sql = f"SELECT user_id FROM notification_member WHERE notification_id = {notification['id']};"
@@ -46,19 +54,21 @@ class NotificationByCategoryAPI(Resource):
             
             database.close()
             return notification_list, 200
-        
+    
+    # 알림 정보 추가
     def post(self, category):
         # Body 데이터 읽어오기
         notification = request.get_json()
         
-        # category 설정
-        notification['category'] = category
-
+        # category, day를 문자열로 변환
+        notification['category'] = convert_to_index(NOTIFICATION_CATEGORY, notification['category'])
+        notification['member_category'] = convert_to_index(MEMBER_CATEGORY, notification['member_category'])
+        notification['day'] = convert_to_index(DAY_CATEGORY, notification['day'])
         database = Database()
 
         # 알림 정보를 DB에 추가
         sql = "INSERT INTO notification "\
-            f"VALUES(NULL, {notification['category']}, '{notification['time']}', "\
+            f"VALUES(NULL, {notification['category']}, {notification['member_category']}, '{notification['time']}', "\
             f"'{notification['start_date']}', '{notification['end_date']}', {notification['day']}, "\
             f"'{notification['cycle']}', '{notification['message']}', '{notification['memo']}');"
         database.execute(sql)
@@ -78,18 +88,21 @@ class NotificationByCategoryAPI(Resource):
 
 @notification.route('/modify/<int:notification_id>')
 class NotificationEditAPI(Resource):
+    # 알림 정보 수정
     def put(self, notification_id):
         # Body 데이터 읽어오기
         notification = request.get_json()
 
-        # category를 index로 변환
+        # category, day를 index로 변환
         notification['category'] = convert_to_index(NOTIFICATION_CATEGORY, notification['category'])
+        notification['member_category'] = convert_to_index(MEMBER_CATEGORY, notification['member_category'])
+        notification['day'] = convert_to_index(DAY_CATEGORY, notification['day'])
 
         database = Database()
 
         # 수정된 알림 정보를 DB에 반영
         sql = "UPDATE notification SET "\
-            f"category = {notification['category']}, time = '{notification['time']}', "\
+            f"category = {notification['category']}, member_category = {notification['member_category']}, time = '{notification['time']}', "\
             f"start_date = '{notification['start_date']}', end_date = '{notification['end_date']}', day = {notification['day']}, "\
             f"cycle = '{notification['cycle']}', message = '{notification['message']}', memo = '{notification['memo']}' "\
             f"WHERE id = {notification_id};"
@@ -109,6 +122,7 @@ class NotificationEditAPI(Resource):
 
         return {'message': '알림 정보를 수정했어요 :)'}, 200
 
+    # 알림 정보 삭제
     def delete(self, notification_id):
         database = Database()
 
@@ -124,3 +138,42 @@ class NotificationEditAPI(Resource):
         database.close()
 
         return {'message': '알림 정보를 삭제했어요 :)'}, 200
+    
+@notification.route('/members')
+class NotificationMemberAPI(Resource):
+    # 회원 목록 얻기
+    def get(self):
+        # DB에서 회원 목록 불러오기
+        database = Database()
+        sql = "SELECT id, name, grade FROM users;"
+        member_list = database.execute_all(sql)
+        database.close()
+        return member_list, 200
+
+@notification.route('/payment-period')
+class NotificationPaymentPeriodAPI(Resource):
+    # 전체 월별 회비 기간 얻기
+    def get(self):
+
+        # 금월 및 작년 6월 날짜 문자열로 얻기
+        current_month = date(datetime.today().year, datetime.today().month, 1).strftime('%Y-%m-%d')
+        start_month = date(datetime.today().year - 1, 6, 1).strftime('%Y-%m-%d')
+
+        # DB에서 월별 회비 납부 기간 불러오기
+        database = Database()
+        sql = f"SELECT date, start_date, end_date FROM monthly_payment_periods "\
+            f"WHERE date between '{start_month}' and '{current_month}' "\
+            f"ORDER BY date;"
+        payment_period_list = database.execute_all(sql)
+        database.close()
+
+        if not payment_period_list: # 납부 기간이 없을 때 처리
+            return {}, 200
+        else:
+            # 납부 기간 내역의 날짜 데이터들을 문자열로 변경
+            for idx, payment_period in enumerate(payment_period_list):
+                payment_period_list[idx]['date'] = payment_period['date'].strftime('%Y-%m-%d')
+                payment_period_list[idx]['start_date'] = payment_period['start_date'].strftime('%Y-%m-%d')
+                payment_period_list[idx]['end_date'] = payment_period['end_date'].strftime('%Y-%m-%d')
+            
+            return payment_period_list, 200
