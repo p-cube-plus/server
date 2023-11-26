2fa0260b247ca62d57ada1f8ff43a4cf66fa2e7f
rhdehgks <rhdehgks@naver.com>
2023-11-26 17:57:29 +0900

    회원 SMS 인증 API 임시 구현

--- api/verification/verification.py
+++ api/verification/verification.py
@@ -1,13 +1,42 @@
 from flask import Flask, request
 from flask_restx import Resource, Namespace
 from database.database import Database
-import sms
+import time, random, sms
 
 verification = Namespace('verification', description='회원 휴대폰 인증')
 
-verification.route("")
-class UserVerificationAPI(Resource):
-    def get(self):
-        pass
+storage = {}
+
+def generate_code(phone_number):
+    code = str(random.randint(0, 999999)).zfill(6)
+    storage[phone_number] = {'code': code, 'time': time.time()}
+    return code
+
+def compare_code(phone_number, code):
+    if phone_number not in storage:
+        return False
+    return storage[phone_number]['code'] == code
+
+verification.route("/request")
+class VerificationRequestAPI(Resource):
     def post(self):
-        pass
+        phone_number = request.get_json()['phone_number']
+
+        code = generate_code()
+        message = f"인증번호: {code}\n 타인 유출로 인한 피해 주의"
+
+        sms.send(phone_number, message)
+
+        return {'message': '인증 번호가 발송되었어요 :)'}, 200
+
+verification.route('/confirm')
+class VerificationConfirmAPI(Resource):
+    def post(self):
+        phone_number = request.get_json()['phone_number']
+        code = request.get_json()['verification_code']
+
+        # 인증 여부 반환 (추후 확장을 고려하여 조건문으로 작성)
+        if compare_code(phone_number, code):
+            return {'is_verified': True}, 200
+        else:
+            return {'is_verified': False}, 200
--- sms/sms_sender.py
+++ sms/sms_sender.py
@@ -1,11 +1,9 @@
 import time
 import requests
 import hmac
-import socket
 import configparser
 import base64
 import hashlib
-import json
 
 __all__ = ['send']
 
@@ -19,14 +17,14 @@ secret_key = config['sms']['sms_secret_key']
 from_number = config['sms']['sms_from_number']
 
 # 공통 헤더 마지막 필드(x-ncp-apigw-signature-v2)에 들어가는 시그니처를 생성
-def _make_signature(self, method, uri, timestamp):
+def _make_signature(method, uri, timestamp):
     message = method + " " + uri + "\n" + timestamp + "\n" + access_key
     message = bytes(message, 'UTF-8')
     signature = base64.b64encode(hmac.new(bytes(secret_key, 'UTF-8'), message, digestmod=hashlib.sha256).digest())
     return signature
 
 # SMS 발송
-def send(self, to_number, message):
+def send(to_number, message):
     timestamp = str(int(time.time() * 1000))
     signature = _make_signature('POST', uri, timestamp)
 
